882af88db3037860c9a9f932068b5bf0cafb48e8
Yanika Sherifova <goyana72@gmail.com>
2024-11-02 05:55:50 +0300

    Add fiber, vite and drei for creating 3d project with react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from "./sections/Navbar"
+import Hero from "./sections/Hero"
+
+function App() {
+
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar />
+      <Hero />
+    </main>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/HackerRoom.jsx
@@ -0,0 +1,8 @@
+
+function HackerRoom() {
+  return (
+    <div>HackerRoom</div>
+  )
+}
+
+export default HackerRoom
