6f92e2b017debe35d4d4aa21e2ef31f56914efda
Yanika Sherifova <goyana72@gmail.com>
2024-12-06 09:37:34 +0300

    Fix CanvasLoader and Add 3d items at the home page

--- vite-project/src/components/CanvasLoader.jsx
+++ vite-project/src/components/CanvasLoader.jsx
@@ -1,26 +1,32 @@
 import { Html, useProgress } from "@react-three/drei";
 
-
 const CanvasLoader = () => {
-    const progress= useProgress();
-    console.log(progress)
-    return (
-        <Html as="div"
-            center
-            style={
-                {
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column"
-                }
-            }
-        >
-            {/* <span className="canvas-loader" /> */}
-            <p style={{ fontSize: 14, color: "#F1F1F1", fontWeight: 800, marginTop: 40 }}>
-                {progress} % loaded</p>
-        </Html>
-    )
+  const { active, progress, errors, item, loaded, total } = useProgress();
+  return (
+    <Html
+      as="div"
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+        <span className="canvas-loader"/>
+      <p
+        className="canvas-loader"
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress !== 0 ? `${progress.toFixed(2)} %` : 'loading...'}
+      </p>
+    </Html>
+  );
 };
 
 export default CanvasLoader;
--- vite-project/src/sections/Hero.jsx
+++ vite-project/src/sections/Hero.jsx
@@ -3,54 +3,18 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import CanvasLoader from "../components/CanvasLoader";
 import HackerRoom from "../components/HackerRoom";
-import { Leva, useControls } from "leva"
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index";
+import Target from "../components/target";
+import ReactLogo from "../components/ReactLogo";
+import Cube from "../components/Cube";
 
 const Hero = () => {
-  const x = useControls("HackerRoom", {
-    //postion 
-    positionX:{
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    positionY:{
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    positionZ:{
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-//rotation
-    rotationX:
-    {
-      value: 0,
-      min: -10,
-      max: 10,
-    },
-    rotationY:
-    {
-      value: 0,
-      min: -10,
-      max: 10,
-    },
-    rotationZ:
-    {
-      value: 0,
-      min: -10,
-      max: 10,
-    },
-    //scale
-    scale:
-    {
-      value: 1,
-      min: 0.1,
-      max: 10,
-    },
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
-  })
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
   return (
     <section className="min-h-screen border-0 w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -61,20 +25,19 @@ const Hero = () => {
           Building Products & Brands
         </p>
         <div className="w-full h-full absolute insert-0">
-          <Leva/>
           <Canvas className="w-full h-full">
-            <Suspense fallBac={<CanvasLoader />}>
-
-              <PerspectiveCamera makeDefault position={[0, 5, 26]} />
+            <Suspense fallback={<CanvasLoader />}>
+              <PerspectiveCamera makeDefault position={[0, -2, 26]} />
               <HackerRoom
-                // scale={0.01} 
-                // position={[2.5, 2.5, 2.5]}
-                // rotation={[0, 0, 0]}
-                position={[x.positionX, x.positionY, x.positionZ]}
-                rotation={[x.rotationX, x.rotationY, x.rotationZ]}
-                scale={[x.scale, x.scale, x.scale]}
+                position={sizes.deskPosition}
+                rotation={[0.2, 3.2, 0.0]}
+                scale={sizes.deskScale}
               />
-
+              <group>
+                <Target position={sizes.targetPosition} />
+                <ReactLogo position={sizes.reactLogoPosition} />
+                <Cube position={sizes.cubePosition}/>
+              </group>
               <ambientLight intensity={1} />
               <directionalLight position={[20, 20, 20]} intensity={0.5} />
             </Suspense>
